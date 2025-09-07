66ceff942bcef2e73de11b04f48083017386059f
Tashi Sherpa <sherpajack3@gmail.com>
2025-09-07 19:31:17 +0545

    Added more functionality to login code

---
+++ Tasi_Scripting/Database/welcome.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+if(!isset($_SESSION['name'])){
+  header('Location: login.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Page</title>
+    <style>
+        body{
+            background-color: lightgreen;
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome Page</h1>
+    <?php
+    echo "Welcome: ". $_SESSION['name'];
+    ?>
+
+    <a href="logout.php">Logout here</a>
+</body>
+</html>
